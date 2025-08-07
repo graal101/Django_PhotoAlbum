c7c0eef7d6c26f440dd509963f29e96503c8c180
Andrew Andrey <graal101@mail.ru>
2025-08-07 14:33:50 +0700

    remove templates/album(app) by the rule

--- album/views.py
+++ album/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
+    return render(request, 'album/index.html' )
+'''
+def index(request):
     return HttpResponse('<h1>Привет мир!</h1>')
+'''
