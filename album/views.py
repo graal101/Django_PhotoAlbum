1509f6002566e6b94924af56fc896d5214a52564
Andrew Andrey <graal101@mail.ru>
2025-08-08 16:33:14 +0700

    added monochrome.html page

--- album/views.py
+++ album/views.py
@@ -6,6 +6,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'album/index.html' )
 
+def monochrome(request):
+    return render(request, 'album/monochrome.html' )
+
 '''
 def index(request):
     return HttpResponse('<h1>Привет мир!</h1>')
