8c3e5dfc21120378dcde514b849c44fefa4d2895
Andrew Andrey <graal101@mail.ru>
2025-08-08 11:46:04 +0700

    add footer, layout.html

--- album/views.py
+++ album/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'album/index.html' )
+
 '''
 def index(request):
     return HttpResponse('<h1>Привет мир!</h1>')
